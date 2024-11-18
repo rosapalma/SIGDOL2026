6731531773f21c1d91b2e0c0e2c7ca25becbf48d
rosapalma <pvicky9586@gmail.com>
2024-11-18 07:37:49 -0400

    traducion de notificaciones VerifEmail

--- resources/views/Administrar/DocsGenerados/index.blade.php
+++ resources/views/Administrar/DocsGenerados/index.blade.php
@@ -2,6 +2,7 @@
     <input type="radio" wire:model.live="list" value="1"  >&nbsp;Constancia(s) de trabajo
     <input type="radio" wire:model.live="list" value="2" style="margin-left: 5%;" >&nbsp;Recibo(s) de Pago
 </div>
+
 <div class="container">
     @include('Administrar.DocsGenerados.filtrar')
     <div class="justify-content-center" >        
